5159ee34405e528f5a6f8dd282695fe5a463b214
khang <enone859@gmail.com>
2024-09-25 07:18:24 -0400

    keymap for no highlight

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "terraform-ls" }
+        ensure_installed = { "lua_ls", "ts_ls", "terraformls" }
       })
     end
   },
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,5 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>', { noremap = true, silent = true })
 vim.g.mapleader = " "

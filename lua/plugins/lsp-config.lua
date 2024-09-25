6d42c72a64cef50f31fa1ffb18e7e62302c6e0c1
khang <enone859@gmail.com>
2024-09-24 21:40:30 -0400

    more stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -8,17 +8,19 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls"  }
+        ensure_installed = { "lua_ls", "ts_ls", "terraform-ls" }
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig");
+      local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.terraformls.setup({})
+
       local opts = {};
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)

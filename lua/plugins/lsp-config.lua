bf2ad2e4ebf0d45c00943486791faeab30febec4
khang <enone859@gmail.com>
2024-09-29 10:14:06 -0400

    git signs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls", "terraformls" },
+        ensure_installed = { "lua_ls", "ts_ls", "terraformls", "cssls" },
       })
     end,
   },
@@ -26,6 +26,9 @@ return {
         capabilities = capabilities,
       })
       lspconfig.terraformls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.cssls.setup({
         capabilities = capabilities,
       })
 

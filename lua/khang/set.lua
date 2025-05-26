ec986514d20023a0d28c26523adaa1fef0b9e340
Khang Nguyen <enone859@gmail.com>
2025-05-26 08:07:20 -0400

    keymapping for buffline

--- lua/khang/set.lua
+++ lua/khang/set.lua
@@ -18,3 +18,19 @@ vim.opt.incsearch = true -- incremental search
  
 vim.opt.termguicolors = true
 
+-- Move to previous/next buffer
+vim.keymap.set('n', '<S-h>', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Prev buffer' })
+vim.keymap.set('n', '<S-l>', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next buffer' })
+
+-- Move buffer left/right
+vim.keymap.set('n', '<A-h>', '<cmd>BufferLineMovePrev<CR>', { desc = 'Move buffer left' })
+vim.keymap.set('n', '<A-l>', '<cmd>BufferLineMoveNext<CR>', { desc = 'Move buffer right' })
+
+-- Close buffer
+vim.keymap.set('n', '<leader>bd', '<cmd>bdelete<CR>', { desc = 'Close buffer' })
+
+-- Pick buffer
+vim.keymap.set('n', '<leader>bp', '<cmd>BufferLinePick<CR>', { desc = 'Pick buffer' })
+
+-- Sort buffers
+vim.keymap.set('n', '<leader>bs', '<cmd>BufferLineSortByDirectory<CR>', { desc = 'Sort buffers by dir' })

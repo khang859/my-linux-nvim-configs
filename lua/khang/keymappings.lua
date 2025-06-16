5978f261eccbc03845a78dd1e701101e9e991868
khang859 <enone859@gmail.com>
2025-06-16 13:20:17 -0400

    Neotree

--- lua/khang/keymappings.lua
+++ lua/khang/keymappings.lua
@@ -5,3 +5,6 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+vim.keymap.set('n', "<C-n>", ":Neotree toggle<CR>")
+vim.keymap.set('n', "<C-h>", ":<C-w>h")
+vim.keymap.set('n', "<C-l>", ":<C-w>l")
